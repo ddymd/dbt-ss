1c20951043b736a480b0d377a220636699ee3054
dongjunjun1013@126.com <dongjunjun1013@126.com>
2023-03-08 07:56:29 +0000

    build first project

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -3,6 +3,13 @@
 -- select * from {{ source('staging', 'green_trips') }}
 -- limit 100
 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by 'vendorid', lpep_pickup_datetime) as rn
+  from {{ source('staging','green_trips') }}
+  where vendorid is not null 
+)
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
@@ -33,8 +40,8 @@ select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
-from {{ source('staging', 'green_trips') }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
 {% if var('is_test_run', default=true) %}
     limit 100
